e90dbf661099fd5801de324deacdd60a6a9297de
Artur Morin <arstmorin@gmail.com>
2022-08-03 17:57:34 +0500

    4 step

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
+import askForName from '../src/cli.js';
 
-import { askForName } from "../src/cli.js";
+console.log('Welcome to the Brain Games!');
 
 askForName();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const askForName = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-};
+export default function askForName() {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+}
